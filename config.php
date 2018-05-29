659e17ed362c12c3c4b086cb404e3a65a3e80eab
Braineee <alindavidsn@yahoo.com>
2018-05-29 10:29:07 +0100

    Intitialize the database connection parameters

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Set the connection to the database here.
+$GLOBALS["config"] = array(
+	"mysql" => array(
+		'host' => 'localhost',// Database host NB: u can also use an ip i.e 127.0.0.1
+		'username' => 'root',// Database user name
+		'password' => '',// Database password 
+		'db' => ''// Name of the database to connect 
+    )
+);
